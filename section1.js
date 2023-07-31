74e3da67a72a16412f53657cb26232e7af16dc4a
vijay pathem <vijaypathem4@gmail.com>
2023-08-01 01:58:04 +0530

    second commit

---
+++ section1.js
@@ -0,0 +1,53 @@
+// challenge 1
+/*
+massMark = 78;
+heightMark = 1.69;
+
+massJohn = 92;
+heightJohn = 1.95;
+
+const markBMI = massMark/(heightMark**2);
+const johnBMI = massJohn/(heightJohn**2);
+
+const markHeigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHeigherBMI);
+*/
+
+
+// challenge 2
+/*
+
+massMark = 78;
+heightMark = 1.69;
+
+massJohn = 92;
+heightJohn = 1.95;
+
+const markBMI = massMark/(heightMark**2);
+const johnBMI = massJohn/(heightJohn**2);
+
+if(markBMI > johnBMI){
+    console.log( `Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
+}else{
+    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}!`)
+}
+
+*/
+
+// challenge 3
+
+/*
+const scoreDolphins = (97+112+101)/3;
+const scoreKoalas = (109+95+123)/3;
+
+if (scoreDolphins > scoreKoalas && scoreDolphins > 100){
+    console.log(`Dolphins Won Game, score : ${scoreDolphins}`);
+}else if (scoreDolphins < scoreKoalas && scoreKoalas > 100){
+    console.log(`Koalas Won Game, score : ${scoreKoalas}`);
+}else if(scoreDolphins === scoreKoalas && scoreDolphins > 100){
+    console.log(` The Game Is Draw. score : ${scoreDolphins}`);
+}else{
+    console.log('No Team Win');
+}
+*/
